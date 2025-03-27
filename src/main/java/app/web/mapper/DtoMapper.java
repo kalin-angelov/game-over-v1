d9b08df44615e36da4eca3bcf3d0e88f5f1db75f
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-03-28 00:38:47 +0200

    Create DtoMapper

---
+++ src/main/java/app/web/mapper/DtoMapper.java
@@ -0,0 +1,28 @@
+package app.web.mapper;
+
+import app.user.model.User;
+import app.web.dto.UserLoginResponse;
+import app.web.dto.UserRegisterResponse;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DtoMapper {
+
+    public static UserRegisterResponse toRegisterResponse(User user) {
+
+        return UserRegisterResponse.builder()
+                .email(user.getEmail())
+                .successful(true)
+                .message("User successfully register.")
+                .build();
+    }
+
+    public static UserLoginResponse toLoginResponse(String token) {
+
+        return UserLoginResponse.builder()
+                .token(token)
+                .successful(true)
+                .message("Welcome")
+                .build();
+    }
+}
