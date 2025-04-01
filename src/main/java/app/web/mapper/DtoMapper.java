502d72d4df55407564c2c9abb03e9354f3ab8701
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-04-01 11:31:31 +0300

    Add toGameResponse mapper in DtoMapper

--- src/main/java/app/web/mapper/DtoMapper.java
+++ src/main/java/app/web/mapper/DtoMapper.java
@@ -1,6 +1,8 @@
 package app.web.mapper;
 
+import app.game.model.Game;
 import app.user.model.User;
+import app.web.dto.GameResponse;
 import app.web.dto.UserLoginResponse;
 import app.web.dto.UserRegisterResponse;
 import lombok.experimental.UtilityClass;
@@ -23,6 +25,15 @@ public class DtoMapper {
                 .token(token)
                 .successful(true)
                 .message("Welcome")
+                .build();
+    }
+
+    public static GameResponse toGameResponse(Game game) {
+
+        return GameResponse.builder()
+                .successful(true)
+                .message("Game successfully created.")
+                .game(game)
                 .build();
     }
 }
