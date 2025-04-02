cb1b31a11ba2cc4890beda0530b2e2535f3c34cf
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-04-02 10:46:06 +0300

    Create NotFoundException

---
+++ src/main/java/app/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package app.exceptions;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
